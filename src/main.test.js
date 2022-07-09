3a135d782f0521f9295880917c4a5ad488754a09
saifiShaddil <shaddilsaifi021296@gmail.com>
2022-07-09 11:30:50 +0530

    testing liberay added

---
+++ src/main.test.js
@@ -0,0 +1,18 @@
+import {describe, expect, it, test} from 'vitest';
+
+
+
+describe('suite name', () => {
+  it('foo', () => {
+    expect(1 + 1).toEqual(2)
+    expect(true).to.be.true
+  })
+
+  it('bar', () => {
+    assert.equal(Math.sqrt(4), 2)
+  })
+
+  it('snapshot', () => {
+    expect({ foo: 'bar' }).toMatchSnapshot()
+  })
+})
